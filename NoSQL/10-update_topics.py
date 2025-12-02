81f277434f6c018d975bcda739af5e6138655968
Adil Valiyev <avliyev17@gmail.com>
2025-12-02 18:13:46 +0000

    degfhy

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Function that changes all topics of a school document based on the name
+"""
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Updates the 'topics' field of a school document identified by its name.
+
+    Args:
+        mongo_collection: pymongo collection object
+        name (str): name of the school to update
+        topics (list of str): list of topics to set
+    """
+    mongo_collection.update_one(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
